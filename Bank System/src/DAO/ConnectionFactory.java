4072ce8e88d6fe05b6ed9948d0d98c2da1afa488
FredericoM28 <madabulafrederico@gmail.com>
2025-10-13 01:18:09 +0200

    Actualizando o nome da base de dasos e a classe da conexao para a DB, Ja esta Funcionando

--- Bank System/src/DAO/ConnectionFactory.java
+++ Bank System/src/DAO/ConnectionFactory.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static final String URL = "jdbc:mysql://localhost:3306/bancoDATABASE?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/bancoDATABSE?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root"; 
     private static final String PASSWORD = "";  // sua senha do MySQL
 
